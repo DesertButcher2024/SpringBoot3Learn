e723ea39c16d77e62ee7e345a41a3cf187553346
zhouguofeng <836747518@qq.com>
2025-08-21 23:38:18 +0800

    code2

---
+++ code/springboot3-02-use-spring-initializr/src/main/java/com/powernode/springboot/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.powernode.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "user system index page!";
+    }
+}
